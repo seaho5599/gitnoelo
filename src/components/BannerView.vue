d95bfb02f0c06ccfbfaaf35218602d73e9d955a4
seaho5599 <seaho5599@gmail.com>
2022-10-11 09:37:04 +0900

    업로드

--- src/components/BannerView.vue
+++ src/components/BannerView.vue
@@ -47,8 +47,8 @@
     SwiperSlide
   } from 'swiper/vue';
   import 'swiper/css';
-  import 'swiper/css/pagination';
-  import "swiper/css/effect-fade";
+    import 'swiper/css/pagination';
+    import "swiper/css/effect-fade";
 
   export default {
     components: {
